3ef5ce41e6afc1d12d3a3052f85caf53221a81f0
Franco-Roldan <francoroldan018@gmail.com>
2023-06-13 22:44:26 -0300

    commit_v5_solucion_errores_al_mostrar_datos

--- js/app.js
+++ js/app.js
@@ -1,24 +1,28 @@
+
 const API_key = '5dd2d9003c1bef8bc95f39d2b5b50327';
-
+const city = 'Barcelona';
 const datos_clima = position => {
+    
+     const {latitude , longitude} = position.coords;
+    //  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_key}`)
+    //      .then(search_data => search_data.json())
+    //      .then(respuesta => console.log(respuesta));
+    
+     //peticion a la api de los datos en el momento, lo devuelve en formato json
+     fetch(`https://api.openweathermap.org/data/2.5/weather?lang=es&units=metric&lat=${latitude}&lon=${longitude}&appid=${API_key}`)
+         .then(respuesta => respuesta.json())
+         .then(datos => set_datos_clima_ahora(datos));
+     // peticion a la api de los datos de los proxiomos 5 dias.
+     fetch(`https://api.openweathermap.org/data/2.5/forecast?lang=es&units=metric&lat=${latitude}&lon=${longitude}&appid=${API_key}`)
+         .then(res_semana => res_semana.json())
+         .then(dato_semana => set_datos_clima_dia(dato_semana))
 
-    const {latitude , longitude} = position.coords;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lang=es&units=metric&lat=${latitude}&lon=${longitude}&appid=${API_key}`)
-        .then(respuesta => respuesta.json())
-        .then(datos => set_datos_clima_ahora(datos));
+ }
+// recolecto del json los datos de interes y los muestro por pantalla
 
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lang=es&units=metric&lat=${latitude}&lon=${longitude}&appid=${API_key}`)
-        .then(res_semana => res_semana.json())
-        .then(dato_semana => set_datos_clima_dia(dato_semana))
-
-}
-
-const set_datos_clima_ahora = datos => {
-    //console.log(datos)
-
+function clima_ahora(datos){
     let temp_redondeada = Math.round(datos.main.temp);
     
-
     const datos_api = {
         ubi: datos.name,
         descripcion: datos.weather[0].description,
@@ -29,21 +33,24 @@ const set_datos_clima_ahora = datos => {
         fecha: get_fecha(),
         sensacion: ('Sensación térmica: ' + Math.round(datos.main.feels_like)+'° C')
     }
+    // tanto las keys del objeto datos_api como los id de las etiquetas en el HTML son iguales
+    // por lo tanto itero el objeto y inserto los valores en su lugar correspondiente 
     Object.keys(datos_api).forEach(key => {
         document.getElementById(key).textContent = datos_api[key]
     })
     icon_clima(datos.weather[0].icon);
 
-    const content = document.getElementById('content');
-    const carga = document.getElementById('loader');
-    carga.style.display = 'none';
-    content.style.display = 'flex';
-
 }
 
+const set_datos_clima_ahora = datos => {
+    //console.log(datos)
+    clima_ahora(datos);
+
+}
+// proximos datos dentro del mismo dia.
 const set_datos_clima_dia = dato_semana => {
     //console.log(dato_semana)
-
+    // 
     const datos_proximos = {
         0:{
             hora: dato_semana.list[0].dt_txt.slice(11,16),
@@ -72,7 +79,7 @@ const set_datos_clima_dia = dato_semana => {
         }
     }
     //console.log(datos_proximos);
-
+    // iteramos e insertamos en el DOM
     for (let i = 0; i < 5; i++) {
         
         let hora = document.getElementById('hr_'+i);
@@ -85,11 +92,11 @@ const set_datos_clima_dia = dato_semana => {
         temp.textContent = datos_proximos[i].temp;
          
     }
-   
+    
     data_week(dato_semana);
 }
 
-function get_fecha(){
+function get_fecha(){ // formato de fecha
     let fecha = new Date();
     return `${fecha.getDate()}-${('0'+ (fecha.getMonth() + 1)).slice(-2)}-${fecha.getFullYear()}`;
 }
@@ -100,12 +107,13 @@ function icon_clima(icon){
     let icono = document.getElementById('img_icon').src = 'https://openweathermap.org/img/w/'+icon+'.png';
     return icono
 }
-
+// datos de la semana
 function data_week(dato_semana){
 
+    
     const dias = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
     let fecha = new String()
-    let Array_fech = [];
+
     let fecha_hoy = new Date()
     fecha_hoy = fecha_hoy.getDay()
 
@@ -114,26 +122,37 @@ function data_week(dato_semana){
 
     for(let i=0; i < dato_semana.list.length; i++){
  
-        
+        // lapso de tiempo de 3 horas
         array_aux = [
-            dato_semana.list[i].dt_txt.slice(0,10),
-            dato_semana.list[i].dt_txt.slice(11,16),
-            'https://openweathermap.org/img/w/'+dato_semana.list[i].weather[0].icon+'.png',
-            (Math.round(dato_semana.list[i].main.temp) + '° C'),
-            (Math.round(dato_semana.list[i].pop * 100)) + '%',
-            (Math.round(dato_semana.list[i].wind.speed)) + 'Km/h'
+            dato_semana.list[i].dt_txt.slice(0,10), // fecha
+            dato_semana.list[i].dt_txt.slice(11,16), // hora
+            'https://openweathermap.org/img/w/'+dato_semana.list[i].weather[0].icon+'.png', // icono
+            (Math.round(dato_semana.list[i].main.temp) + '° C'), // temp
+            (Math.round(dato_semana.list[i].pop * 100)) + '%', // prob de lluvia
+            (Math.round(dato_semana.list[i].wind.speed)) + 'Km/h' // viento
         ];
-        dias_semana.push(array_aux);
+        dias_semana.push(array_aux); 
     }
+    //console.log(dias_semana);
     let aux = 0;
+    let fecha_init = new Date(dias_semana[0][0]);
+    fecha_init = fecha_init.getDay() + 1;
+    //console.log(fecha_init, fecha_hoy);
     for(let i=0; i < dias_semana.length; i++){
-      
+
         let fecha_dia = new Date(dias_semana[i][0]);
         fecha = fecha_dia.getDay() + 1; 
         
-        if(fecha != fecha_hoy){
+        if(fecha != fecha_hoy){ // si la fecha del dato es distinta a la de hoy
             
-            if(dias_semana[i][0] == dias_semana[i - 1][0]){
+            if(fecha_init == (fecha_hoy + 1)){
+                dias_semana.unshift([dias_semana[i][0]]);
+                fecha_init = 0;
+                aux=0;
+                i++;
+            }
+           
+            if(dias_semana[i][0] == dias_semana[i - 1][0]){ // si la fecha del dato es igual a la fecha anterior
                 //console.log(dias_semana[i][0]+ ' - '+dias_semana[i][1]);
                 
                 let ul = document.createElement('ul');
@@ -161,6 +180,12 @@ function data_week(dato_semana){
 
 
             }else{
+
+                if(fecha_init == 0){
+                    let p = document.getElementById('p0');
+                    p.textContent = dias[fecha_hoy+1];
+                    fecha_init =1;
+                }
                 switch (fecha) {
                     case fecha_hoy + 1:
                         aux = 0;
@@ -176,17 +201,22 @@ function data_week(dato_semana){
                         break;
                     case fecha_hoy + 5:
                         aux = 4;
+                        break;
                     default:
-                        aux++
+                        aux = 5;
                         break;
                 }
 
                 //console.log( 'p'+aux + ' - '+dias[fecha] + ' - '+fecha);
+                //console.log(fecha, aux)
                 if(fecha == 7){
                     fecha = 0;
                 }
+                
+                //console.log('p'+aux);
                 let p = document.getElementById('p'+aux);
-            
+                
+                //console.log(dias[fecha], dias_semana[i][0])
                 p.textContent = dias[fecha];
             
                 let ul = document.createElement('ul');
@@ -213,20 +243,17 @@ function data_week(dato_semana){
                 day.appendChild(ul);
             }
             
-
-           
-                
-            
         }
-
-    
-        
-
     }
 
 
     //console.log(dias_semana);
 
+    // una vez cargado todo el contenido oculto el simbolo de carga y muestro el contenido
+    const content = document.getElementById('content');
+    const carga = document.getElementById('loader');
+    carga.style.display = 'none';
+    content.style.display = 'flex';
 }
 
 
